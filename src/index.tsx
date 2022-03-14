067c5742529f6f969f413f2805d0280607ac6cba
ccxz84 <ccxz84@naver.com>
2022-03-14 22:35:27 +0900

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 ReactDOM.render(
-  //  <Word documentId={Number(4786)} fileName="테스트워드(신철헌)" timeStamp="000000" userId={1111} />,
   <div />,
   document.getElementById('root')
 );
